3c06505bcb289e67c1b83a972985df020f803279
hung06031998 <hungnv6398@gmail.com>
2019-12-05 15:40:19 +0700

    handler error

--- Server/server.c
+++ Server/server.c
@@ -14,197 +14,195 @@
 
 const int _listenQueue = 1024;  // Backlog in listen()
 const int _bufferLength = 1024;
-const char *_terminateChar = "@logout";
 
-
-char *firstClientAddr = NULL;
-char *firstClientAddrTmp = NULL;
-bool isFileSent = false;
+char* lastClientAddr = NULL;
+char* lastClientAddrTmp = NULL;
 
 char fileName[1024] = "";
 
 int connectedClient = 0;
-int clientReceived = 0;
-int nameCount = 0;
+int receivedClient = 0;
+int receivedNameClient = 0;
+bool isFileSent = false;
 bool allReceived = true;
+
 pthread_mutex_t mptr_clientCount = PTHREAD_MUTEX_INITIALIZER;
 
 void showTime() {
-    time_t mytime = time(NULL);
-    char * time_str = ctime(&mytime);
-    time_str[strlen(time_str)-1] = '\0';
-    printf("Current Time : %s\n", time_str);
+	time_t mytime = time(NULL);
+	char* time_str = ctime(&mytime);
+	time_str[strlen(time_str) - 1] = '\0';
+	printf("Current Time : %s\n", time_str);
 }
 
-void *handleRequest(void *arg) {
-    int connClientSocket = *((int *) arg);
-    free(arg);
-    pthread_detach(pthread_self());
+void resetStatus() {
+    bzero(fileName, sizeof(fileName));
+    printf("\nEnter file name to send: ");
+    scanf("%s", fileName);
+    showTime();
+    isFileSent = false;
+    receivedClient = 0;
+    receivedNameClient = 0;
+    allReceived = true;
+}
 
-    // Receive file name from client and send file back
-    char buffer[_bufferLength];
-    int downloadType = 0;
+void* handleRequest(void* arg) {
+	int connClientSocket = *((int*)arg);
+	free(arg);
+	pthread_detach(pthread_self());
 
-    if (connectedClient == 3) {
-        printf("\nEnter file name to send: ");
-        scanf("%s", fileName);
-        showTime();
-    }
-    while (1) {
-        if (connectedClient == 3 && strcmp(fileName, "") != 0) break;
-    }
-    while (1) {
-        if (!allReceived) continue;
-        write(connClientSocket, fileName, sizeof(fileName));
-        //int nbytes = write(connClientSocket, fileName, sizeof(fileName));
-        //printf("Nbytes: %d\n", nbytes);
-        pthread_mutex_lock(&mptr_clientCount);
-        nameCount++;
-        pthread_mutex_unlock(&mptr_clientCount);
-        if (firstClientAddr == NULL || downloadType == 1) {
-            firstClientAddr = firstClientAddrTmp;
-            downloadType = 1;
-            write(connClientSocket, &downloadType, sizeof(downloadType));
-            while (1) {
-                if (nameCount == 3) break;
-            }
-            FILE *file = fopen(fileName, "rb");
-            int size;
-            // Handle error
-            if (file == NULL) {
-                size = -1;
-                printf("Cannot find file '%s'!\n", fileName);
-                size = htonl(size);
-                write(connClientSocket, (void *) &size, sizeof(size));
-            }
-            else {
-                printf("Sending file '%s'\n", fileName);
-                // Send file length first
-                fseek(file, 0, SEEK_END);
-                size = ftell(file);
-                fseek(file, 0, SEEK_SET);
-                printf("File size: %d\n", size);
-                size = htonl(size);
-                write(connClientSocket, (void *) &size, sizeof(size));
-                // Send file
-                while (!feof(file)) {
-                    // Read file to buffer
-                    int readSize = fread(buffer, 1, sizeof(buffer) - 1, file);
-                    write(connClientSocket, buffer, readSize);
-                    // Zero out buffer after writing
-                    bzero(buffer, sizeof(buffer));
-                    //printf ("readSize: %d\n", readSize);
-                isFileSent = true;
-                }
-                fclose(file);
-                printf("Sent file successfully!\n");
-            }
-        }
-        else {
-            downloadType = 2;
-            write(connClientSocket, &downloadType, sizeof(downloadType));
-            write(connClientSocket, firstClientAddr, _bufferLength);
-            while (1) {
-                //DO NOTHING
-                if (isFileSent) break;
-            }
-        }
+	char buffer[_bufferLength];
+	int downloadType = 0;
 
-        if (nameCount == 3) {
-            allReceived = false;
-        }
-        int done = 0;
-        read (connClientSocket, &done, sizeof(done));
-        if (done == 1) {
-            pthread_mutex_lock(&mptr_clientCount);
-            clientReceived++;
-            //printf("Total Clients Received: %d\n", clientReceived);
-            pthread_mutex_unlock(&mptr_clientCount);
-        }
+	if (connectedClient == 3) {
+		printf("\nEnter file name to send: ");
+		scanf("%s", fileName);
+		showTime();
+	}
+	while (1) {
+		if (connectedClient == 3 && strcmp(fileName, "") != 0) break;
+	}
+	while (1) {
+		if (!allReceived) continue;
+		write(connClientSocket, fileName, sizeof(fileName));
+		pthread_mutex_lock(&mptr_clientCount);
+		receivedNameClient++;
+		pthread_mutex_unlock(&mptr_clientCount);
+		if (lastClientAddr == NULL || downloadType == 1) {
+			lastClientAddr = lastClientAddrTmp;
+			downloadType = 1;
+			write(connClientSocket, &downloadType, sizeof(downloadType));
+			while (1) {
+				if (receivedNameClient == 3) break;
+			}
+			FILE* file = fopen(fileName, "rb");
+			int size;
+			// Handle error
+			if (file == NULL) {
+				size = -1;
+				printf("Cannot find file '%s'!\n", fileName);
+				size = htonl(size);
+				write(connClientSocket, (void*)&size, sizeof(size));
+				isFileSent = true;
+			}
+			else {
+				printf("Sending file '%s'\n", fileName);
+				// Send file length first
+				fseek(file, 0, SEEK_END);
+				size = ftell(file);
+				fseek(file, 0, SEEK_SET);
+				printf("File size: %d\n", size);
+				size = htonl(size);
+				write(connClientSocket, (void*)&size, sizeof(size));
+				// Send file
+				while (!feof(file)) {
+					// Read file to buffer
+					int readSize = fread(buffer, 1, sizeof(buffer) - 1, file);
+					write(connClientSocket, buffer, readSize);
+					// Zero out buffer after writing
+					bzero(buffer, sizeof(buffer));
+					//printf ("readSize: %d\n", readSize);
+					isFileSent = true;
+				}
+				fclose(file);
+				printf("Sent file successfully!\n");
+			}
+		}
+		else {
+			downloadType = 2;
+			write(connClientSocket, &downloadType, sizeof(downloadType));
+			write(connClientSocket, lastClientAddr, _bufferLength);
+			while (1) {
+				//DO NOTHING
+				if (isFileSent) break;
+			}
+		}
 
-        if (clientReceived == 3) {
-            bzero(fileName, sizeof(fileName));
-            printf("\nEnter file name to send: ");
-            scanf("%s", fileName);
-            showTime();
-            //firstClientAddr = NULL;
-            isFileSent = false;
-            clientReceived = 0;
-            nameCount = 0;
-            allReceived = true;
-        }
-    }
-    close(connClientSocket);
-    return NULL;
+		if (receivedNameClient == 3) {
+			allReceived = false;
+		}
+		int done = 0;
+		read(connClientSocket, &done, sizeof(done));
+		if (done == 1) {
+			pthread_mutex_lock(&mptr_clientCount);
+			receivedClient++;
+			//printf("Total Clients Received: %d\n", clientReceived);
+			pthread_mutex_unlock(&mptr_clientCount);
+		}
+
+		if (receivedClient == 3) {
+            resetStatus();
+		}
+	}
+	close(connClientSocket);
+	return NULL;
 }
 
 int main() {
-    const int _family = AF_INET;    // IPv4
-    const int _type = SOCK_STREAM;  // TCP
-    const int _protocol = 0;
-    const int _port = 8080;
+	const int _family = AF_INET;    // IPv4
+	const int _type = SOCK_STREAM;  // TCP
+	const int _protocol = 0;
+	const int _port = 8080;
 
-    int serverSocket;
-    int *connClientSocket;
-    struct sockaddr_in serverAddr, connClientAddr;
+	int serverSocket;
+	int* connClientSocket;
+	struct sockaddr_in serverAddr, connClientAddr;
 
-    // Create a socket to listen
-    serverSocket = socket(_family, _type, _protocol);
-    if (serverSocket < 0) {
-        perror("Server socket error");
-        return 1;
-    }
+	// Create a socket to listen
+	serverSocket = socket(_family, _type, _protocol);
+	if (serverSocket < 0) {
+		perror("Server socket error");
+		return 1;
+	}
 
-    int option = 1;
-    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
+	int option = 1;
+	setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
 
+	bzero(&serverAddr, sizeof(serverAddr));
 
-    bzero(&serverAddr, sizeof(serverAddr));
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_port = htons(_port);
+	serverAddr.sin_family = _family;
 
-    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddr.sin_port = htons(_port);
-    serverAddr.sin_family = _family;
+	int bindCheck = bind(serverSocket, (struct sockaddr*) & serverAddr, sizeof(serverAddr));
+	if (bindCheck < 0) {
+		perror("Bind error");
+		return 1;
+	}
 
-    int bindCheck = bind(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
-    if (bindCheck < 0) {
-        perror("Bind error");
-        return 1;
-    }
+	int listenCheck = listen(serverSocket, _listenQueue);
+	if (listenCheck < 0) {
+		perror("Listen error");
+		return 1;
+	}
 
-    int listenCheck = listen(serverSocket, _listenQueue);
-    if (listenCheck < 0) {
-        perror("Listen error");
-        return 1;
-    }
+	printf("Server is listening at port %d. Waiting for connection...\n", _port);
 
-    printf("Server is listening at port %d. Waiting for connection...\n", _port);
+	unsigned int addrLength = sizeof(connClientAddr);
 
-    unsigned int addrLength = sizeof(connClientAddr);
+	while (1) {
+		connClientSocket = malloc(sizeof(int));
+		*connClientSocket = accept(serverSocket, (struct sockaddr*) & connClientAddr, &addrLength);
+		if (connClientSocket < 0) {
+			if (errno == EINTR) continue;
+			else {
+				perror("Accept error");
+				return 1;
+			}
+		}
+		// Get client info
+		char* clientIpAddr = inet_ntoa(connClientAddr.sin_addr);
+		lastClientAddrTmp = inet_ntoa(connClientAddr.sin_addr);
+		int clientPort = ntohs(connClientAddr.sin_port);
 
-    while (1) {
-        connClientSocket = malloc(sizeof(int));
-        *connClientSocket = accept(serverSocket, (struct sockaddr *) &connClientAddr, &addrLength);
-        if (connClientSocket < 0) {
-            if (errno == EINTR) continue;
-            else {
-                perror("Accept error");
-                return 1;
-            }
-        }
-        // Get client info
-        char *clientIpAddr = inet_ntoa(connClientAddr.sin_addr);
-        firstClientAddrTmp = inet_ntoa(connClientAddr.sin_addr);
-        int clientPort = ntohs(connClientAddr.sin_port);
+		printf("Client address: %s:%d\n", clientIpAddr, clientPort);
+		connectedClient++;
 
-        printf("Client address: %s:%d\n", clientIpAddr, clientPort);
-        connectedClient++;
+		// Create a thread to handle requests of client
+		pthread_t tid;
+		pthread_create(&tid, NULL, &handleRequest, (void*)connClientSocket);
+	}
 
-        // Create a thread to handle requests of client
-        pthread_t tid;
-        pthread_create(&tid, NULL, &handleRequest, (void *) connClientSocket);
-    }
-
-    close(serverSocket);
-    return 0;
+	close(serverSocket);
+	return 0;
 }
-
